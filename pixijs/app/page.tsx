5fac3fec78fd3f0b8e023cba8c2b975998384f66
stan-dot <stanislawmalinowski09@gmail.com>
2023-02-16 08:36:56 +0000

    cutting my losses

--- pixijs/app/page.tsx
+++ pixijs/app/page.tsx
@@ -1,15 +1,25 @@
+"use client";
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
+import { useEffect } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  useEffect(() => {
+    const app = require("./PixiGame");
+
+    return () => {
+    }
+  }, [])
+
   return (
     <main className={styles.main}>
       <h1 className='text-3xl font-bold underline'>
         Hello world!
       </h1>
+      <canvas id="pixi-canvas"></canvas>
     </main>
   )
 }
